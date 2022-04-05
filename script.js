44401bc16cfa40a5476d1bc994f910e9f0362b6c
zahidur0 <88269984+zahidur0@users.noreply.github.com>
2022-04-05 13:38:09 +0100

    Add basic click event

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "😡 No number!";
+  }
+});
